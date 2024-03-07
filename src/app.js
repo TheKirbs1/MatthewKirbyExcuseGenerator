f177b8a58cd3ddb4d1b0a14dc3069aa4517b0cbe
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-03-07 16:57:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  let generateExcuse = function() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    const getRandomIndex = array => {
+      return Math.floor(Math.random() * array.length);
+    };
+
+    return (
+      who[getRandomIndex(who)] +
+      " " +
+      action[getRandomIndex(action)] +
+      " " +
+      what[getRandomIndex(what)] +
+      " " +
+      when[getRandomIndex(when)]
+    );
+  };
 };
